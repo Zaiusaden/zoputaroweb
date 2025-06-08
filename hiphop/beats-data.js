8d2cb06e899de59345ef1ff620f8611d8b9ac82d
Zaiusaden <zoputaro@hotmail.com>
2025-06-08 20:41:44 +0200

    Update beats-data.js

--- hiphop/beats-data.js
+++ hiphop/beats-data.js
@@ -3,7 +3,7 @@ const beats = [
         id: 'beat1',
         title: 'Beat1',
         type: 'Boom Bap',
-        file: 'beats/Beat1.ogg',
+        file: 'beats/Beat1.mp3',
         bpm: 92,
         category: 'boom-bap'
     },
@@ -11,7 +11,7 @@ const beats = [
         id: 'beat2',
         title: 'Beat2',
         type: 'Boom Bap',
-        file: 'beats/Beat2.ogg',
+        file: 'beats/Beat2.mp3',
         bpm: 100,
         category: 'boom-bap'
     },
@@ -19,7 +19,7 @@ const beats = [
         id: 'beat3',
         title: 'Beat3',
         type: 'Boom Bap',
-        file: 'beats/Beat3.ogg',
+        file: 'beats/Beat3.mp3',
         bpm: 95,
         category: 'boom-bap'
     },
@@ -27,7 +27,7 @@ const beats = [
         id: 'beat4',
         title: 'Beat4',
         type: 'Boom Bap',
-        file: 'beats/Beat4.ogg',
+        file: 'beats/Beat4.mp3',
         bpm: 90,
         category: 'boom-bap'
     },
@@ -35,7 +35,7 @@ const beats = [
         id: 'beat5',
         title: 'Beat5',
         type: 'Boom Bap',
-        file: 'beats/Beat5.ogg',
+        file: 'beats/Beat5.mp3',
         bpm: 90,
         category: 'boom-bap'
     },
@@ -43,7 +43,7 @@ const beats = [
         id: 'beat6',
         title: 'Beat6',
         type: 'Boom Bap',
-        file: 'beats/Beat6.ogg',
+        file: 'beats/Beat6.mp3',
         bpm: 87,
         category: 'boom-bap'
     },
@@ -51,7 +51,7 @@ const beats = [
         id: 'beat7',
         title: 'Beat7',
         type: 'Boom Bap',
-        file: 'beats/Beat7.ogg',
+        file: 'beats/Beat7.mp3',
         bpm: 87.5,
         category: 'boom-bap'
     },
@@ -59,7 +59,7 @@ const beats = [
         id: 'beat8',
         title: 'Beat8',
         type: 'Boom Bap',
-        file: 'beats/Beat8.ogg',
+        file: 'beats/Beat8.mp3',
         bpm: 91,
         category: 'boom-bap'
     },
@@ -67,7 +67,7 @@ const beats = [
         id: 'beat9',
         title: 'Beat9',
         type: 'Boom Bap',
-        file: 'beats/Beat9.ogg',
+        file: 'beats/Beat9.mp3',
         bpm: 89,
         category: 'boom-bap'
     },
@@ -75,7 +75,7 @@ const beats = [
         id: 'beat10',
         title: 'Beat10',
         type: 'Boom Bap',
-        file: 'beats/Beat10.ogg',
+        file: 'beats/Beat10.mp3',
         bpm: 96,
         category: 'boom-bap'
     },
@@ -83,7 +83,7 @@ const beats = [
         id: 'beat11',
         title: 'Beat11',
         type: 'Boom Bap',
-        file: 'beats/Beat11.ogg',
+        file: 'beats/Beat11.mp3',
         bpm: 92,
         category: 'boom-bap'
     },
@@ -91,7 +91,7 @@ const beats = [
         id: 'beat12',
         title: 'Beat12',
         type: 'Trap/Drill',
-        file: 'beats/Beat12.ogg',
+        file: 'beats/Beat12.mp3',
         bpm: 69,
         category: 'trap-drill'
     },
@@ -99,7 +99,7 @@ const beats = [
         id: 'beat13',
         title: 'Beat13',
         type: 'Boom Bap',
-        file: 'beats/Beat13.ogg',
+        file: 'beats/Beat13.mp3',
         bpm: 90,
         category: 'boom-bap'
     },
@@ -107,7 +107,7 @@ const beats = [
         id: 'beat14',
         title: 'Beat14',
         type: 'Boom Bap',
-        file: 'beats/Beat14.ogg',
+        file: 'beats/Beat14.mp3',
         bpm: 97,
         category: 'boom-bap'
     },
@@ -115,7 +115,7 @@ const beats = [
         id: 'beat15',
         title: 'Beat15',
         type: 'Boom Bap',
-        file: 'beats/Beat15.ogg',
+        file: 'beats/Beat15.mp3',
         bpm: 90,
         category: 'boom-bap'
     },
@@ -123,7 +123,7 @@ const beats = [
         id: 'beat16',
         title: 'Beat16',
         type: 'Trap/Drill',
-        file: 'beats/Beat16.ogg',
+        file: 'beats/Beat16.mp3',
         bpm: 71,
         category: 'trap-drill'
     },
@@ -131,7 +131,7 @@ const beats = [
         id: 'beat17',
         title: 'Beat17',
         type: 'Trap/Drill',
-        file: 'beats/Beat17.ogg',
+        file: 'beats/Beat17.mp3',
         bpm: 72,
         category: 'trap-drill'
     },
@@ -139,7 +139,7 @@ const beats = [
         id: 'beat18',
         title: 'Beat18',
         type: 'Trap/Drill',
-        file: 'beats/Beat18.ogg',
+        file: 'beats/Beat18.mp3',
         bpm: 71,
         category: 'trap-drill'
     },
@@ -147,7 +147,7 @@ const beats = [
         id: 'beat19',
         title: 'Beat19',
         type: 'Trap/Drill',
-        file: 'beats/Beat19.ogg',
+        file: 'beats/Beat19.mp3',
         bpm: 71,
         category: 'trap-drill'
     },
@@ -155,7 +155,7 @@ const beats = [
         id: 'beat20',
         title: 'Beat20',
         type: 'Trap/Drill',
-        file: 'beats/Beat20.ogg',
+        file: 'beats/Beat20.mp3',
         bpm: 73,
         category: 'trap-drill'
     },
@@ -163,7 +163,7 @@ const beats = [
         id: 'beat21',
         title: 'Beat21',
         type: 'Trap/Drill',
-        file: 'beats/Beat21.ogg',
+        file: 'beats/Beat21.mp3',
         bpm: 70,
         category: 'trap-drill'
     },
@@ -171,7 +171,7 @@ const beats = [
         id: 'beat22',
         title: 'Beat22',
         type: 'Boom Bap',
-        file: 'beats/Beat22.ogg',
+        file: 'beats/Beat22.mp3',
         bpm: 90,
         category: 'boom-bap'
     },
@@ -179,7 +179,7 @@ const beats = [
         id: 'beat23',
         title: 'Beat23',
         type: 'Boom Bap',
-        file: 'beats/Beat23.ogg',
+        file: 'beats/Beat23.mp3',
         bpm: 101,
         category: 'boom-bap'
     },
@@ -187,7 +187,7 @@ const beats = [
         id: 'beat24',
         title: 'Beat24',
         type: 'Trap/Drill',
-        file: 'beats/Beat24.ogg',
+        file: 'beats/Beat24.mp3',
         bpm: 74,
         category: 'trap-drill'
     },
@@ -195,7 +195,7 @@ const beats = [
         id: 'beat25',
         title: 'Beat25',
         type: 'Trap/Drill',
-        file: 'beats/Beat25.ogg',
+        file: 'beats/Beat25.mp3',
         bpm: 73,
         category: 'trap-drill'
     },
@@ -203,7 +203,7 @@ const beats = [
         id: 'beat26',
         title: 'Beat26',
         type: 'Boom Bap',
-        file: 'beats/Beat26.ogg',
+        file: 'beats/Beat26.mp3',
         bpm: 90,
         category: 'boom-bap'
     },
@@ -211,7 +211,7 @@ const beats = [
         id: 'beat27',
         title: 'Beat27',
         type: 'Boom Bap',
-        file: 'beats/Beat27.ogg',
+        file: 'beats/Beat27.mp3',
         bpm: 86,
         category: 'boom-bap'
     },
@@ -219,10 +219,32 @@ const beats = [
         id: 'beat28',
         title: 'Beat28',
         type: 'Boom Bap',
-        file: 'beats/Beat28.ogg',
+        file: 'beats/Beat28.mp3',
         bpm: 86,
         category: 'boom-bap'
+    },
+    {
+        id: 'beat29',
+        title: 'Beat29',
+        type: 'Trap/Drill',
+        file: 'beats/Beat29.mp3',
+        bpm: 73,
+        category: 'trap-drill'
+    },
+    {
+        id: 'beat30',
+        title: 'Beat30',
+        type: 'Trap/Drill',
+        file: 'beats/Beat30.mp3',
+        bpm: 75,
+        category: 'trap-drill'
+    },
+    {
+        id: 'beat31',
+        title: 'Beat31',
+        type: 'Trap/Drill',
+        file: 'beats/Beat31.mp3',
+        bpm: 75,
+        category: 'trap-drill'
     }
 ];
-
-console.log('Beats-data.js cargado correctamente ✅');
