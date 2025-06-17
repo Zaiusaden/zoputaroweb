f3ef2e1753503a41fd705e8215971c22c798be67
Zaiusaden <zoputaro@hotmail.com>
2025-06-17 19:40:16 +0200

    Delete hiphop/librarycratez/js/app-config.js

--- hiphop/librarycratez/js/app-config.js
+++
@@ -1,6 +0,0 @@
-const AppConfig = {
-    palettes: ['default', 'neon', 'fire', 'ocean', 'earth', 'cyber', 'sunset', 'violet', 'gold', 'plasma'],
-    defaultPalette: 'ocean',
-    defaultLanguage: 'es',
-    defaultVolume: 70
-};
